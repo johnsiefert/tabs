a77efea0f89ed86a19bdfa5a974ec374c9f201df
John <johnsief12@gmail.com>
2024-02-01 17:48:25 -0500

    completed the project where you can tab through the buttons

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 const content = document.querySelectorAll('.content');
 const tab = document.querySelectorAll('.tab-btn');
+const about = document.querySelector('.about');
+
+//my way of building the project
+// tab.forEach(function (btn) {
+//   btn.addEventListener('click', function (e) {
+//             const tabs = e.currentTarget.dataset.id
+//     content.forEach(function(item){
+
+//         if(item.getAttribute('id') === tabs){
+//             item.classList.add('active');
+//         }else {
+//             item.classList.remove('active');
+//         }
+
+//     });
+//   });
+// });
 
 
-
-tab.forEach(function (btn) {
-  btn.addEventListener('click', function (e) {
-            const tabs = e.currentTarget.dataset.id
-    content.forEach(function(item){
-
-        if(item.getAttribute('id') === tabs){
-            item.classList.add('active');
-        }else {
-            item.classList.remove('active');
-        }
-
+//tutorial way to build the project
+about.addEventListener('click', function(e){
+const id = e.target.dataset.id;
+if(id){
+    //remove activate from other buttons
+    tab.forEach(function(btn){
+        btn.classList.remove('active');
+        e.target.classList.add('active');
     });
-  });
+    //hide other articles
+    content.forEach(function(article){
+        article.classList.remove('active');
+    });
+    const element = document.getElementById(id);
+    element.classList.add('active');
+}
 });
